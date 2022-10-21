0f739f543fc3093ba14bd87fbfab0cc4f68e6b40
shinasvb19 <myofficial.shinas@gmail.com>
2022-10-21 14:03:41 +0530

    mail_commit

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,32 +2,42 @@ const Cart = require("../models/cartSchema");
 const mongoose = require('mongoose');
 
 const cartPage = async (req, res) => {
-    const session = req.session.username
-    const cartFind = await Cart.find({})
-    const cartAgregate = await Cart.aggregate([{
-        $lookup: {
-            from: 'categories',
-            localField: 'category_id', foreignField: '_id', as: 'category'
-        }
-    },
-    {
-        $lookup: {
-            from: 'subcategories',
-            localField: 'subcategory_id', foreignField: '_id', as: 'subcategory'
-        }
+    let session = req.session.user_id
+    session = mongoose.Types.ObjectId(session);
+    // const cardItem = await Cart.findOne({ user_id: session })
 
-    }])
-    res.render('user/cart', { session })
+    const cartAgregate = await Cart.aggregate([
+        { $match: { user_id: session } },
+        { $unwind: '$cart_item' },
+        {
+            $project:
+            {
+                product_id: '$cart_item.product_id',
+                itemQuantity: '$cart_item.product_quantity',
+                size: '$cart_item.product_size'
+            }
+        },
+
+        {
+            $lookup: {
+                from: 'products',
+                localField: 'product_id', foreignField: '_id', as: 'products'
+            }
+        }
+    ])
+    console.log(cartAgregate)
+
+    res.render('user/cart', { session, cartAgregate })
 }
 
 const cartPost = async (req, res) => {
     let user_id = req.session.user_id;
     user_id = mongoose.Types.ObjectId(user_id);
-    console.log(req.body)
-    console.log(req.session)
+    // console.log(req.body)
+    // console.log(req.session)
 
     if (user_id) {
-        console.log("   user id    ", user_id);
+        // console.log("   user id    ", user_id);
         const { product_id, product_size, product_quantity } = req.body;
 
 
@@ -36,7 +46,7 @@ const cartPost = async (req, res) => {
 
             let productx = await Cart.findOne({ $and: [{ user_id }, { cart_item: { $elemMatch: { product_id, product_size } } }] });
             if (productx) {
-                console.log("use exits", productx)
+                // console.log("use exits", productx)
                 await Cart.findOneAndUpdate({ $and: [{ user_id }, { "cart_item.product_id": product_id }, { "cart_item.product_size": product_size }] }, { $inc: { "cart_item.$.product_quantity": product_quantity } });
                 //  console.log("prouct exites"     , productx.user_id);
             } else {
@@ -59,7 +69,24 @@ const cartPost = async (req, res) => {
         res.redirect("/login");
     }
 }
+const quantity = async (req, res) => {
+    let user_id = req.session.user_id;
+    user_id = mongoose.Types.ObjectId(user_id);
+    let {cart, product ,size , count} = req.body;
+    const product_id = product;
+    const  product_size = size;
+    await  Cart.findOneAndUpdate({ $and: [{ user_id }, { "cart_item.product_id": product_id }, { "cart_item.product_size": product_size }] }, { $inc: { "cart_item.$.product_quantity": count } });
+    //  console.log("prouct exites"     , productx.user_id);;
+}
+const cartItemDelete = async (req,res) =>{
+    // console.log('here now..',req.body)
+    const productId = new mongoose.Types.ObjectId(req.body.product);
+    let user_id= (req.session.user_id);
+    // console.log(productId)
+    await Cart.updateOne({user_id},{$pull:{cart_item:{"product_id":productId}}});
+}
 
-
+exports.cartItemDelete = cartItemDelete; 
+exports.quantity = quantity;
 exports.cartPost = cartPost;
 exports.cartPage = cartPage;
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,7 +3,8 @@ const cartController = require("../controllers/cartController");
 const router = express.Router();
 router.route('/')
     .get(cartController.cartPage)
-    .post(cartController.cartPost);
-
+    .post(cartController.cartPost)
+router.post('/change-product-qt', cartController.quantity)
+router.post('/cartItemDelete',cartController.cartItemDelete)
 
 module.exports = router;
